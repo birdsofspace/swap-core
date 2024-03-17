1b888fe360b84b0638f2a0cfa14f91b67962cdd5
Birds of Space <kelvin@birdsofspace.com>
2024-03-18 05:02:52 +0700

    Developer includes Core Swap Token contract

--- README.md
+++ README.md
@@ -17,3 +17,28 @@
 + Factory deployed to : 0xde57CDA805f01c1647c4b6fc6FFdAF36AE6f6600
 + Router V02 deployed to :  0x5b0AB9AFe2e5a6eA801fe93BF65478d5A2f8e903
 + Multicall deployed to : 0xBD6a373E0c80fF321FbD4A49e35527833040bf66
+
+## Binance Smart Chain Testnet
++ BETHER deployed to : 0xD6Ad289eEb37d7dd7A3D836D68D1f8Dd8708b5D9
++ Factory deployed to : 0xde57CDA805f01c1647c4b6fc6FFdAF36AE6f6600
++ Router V02 deployed to :  0x5b0AB9AFe2e5a6eA801fe93BF65478d5A2f8e903
++ Multicall deployed to : 0xBD6a373E0c80fF321FbD4A49e35527833040bf66
+
+## Fantom Testnet
++ BETHER deployed to : 0xD6Ad289eEb37d7dd7A3D836D68D1f8Dd8708b5D9
++ Factory deployed to : 0xde57CDA805f01c1647c4b6fc6FFdAF36AE6f6600
++ Router V02 deployed to :  0x5b0AB9AFe2e5a6eA801fe93BF65478d5A2f8e903
++ Multicall deployed to : 0xBD6a373E0c80fF321FbD4A49e35527833040bf66
+
+## Polygon Testnet (Mumbai)
++ BETHER deployed to : 0xD6Ad289eEb37d7dd7A3D836D68D1f8Dd8708b5D9
++ Factory deployed to : 0xde57CDA805f01c1647c4b6fc6FFdAF36AE6f6600
++ Router V02 deployed to :  0x5b0AB9AFe2e5a6eA801fe93BF65478d5A2f8e903
++ Multicall deployed to : 0xBD6a373E0c80fF321FbD4A49e35527833040bf66
+
+## Polygon Mainnet
++ BETHER deployed to : 0xD6Ad289eEb37d7dd7A3D836D68D1f8Dd8708b5D9
++ Factory deployed to : 0xde57CDA805f01c1647c4b6fc6FFdAF36AE6f6600
++ Router V02 deployed to :  0x5b0AB9AFe2e5a6eA801fe93BF65478d5A2f8e903
++ Multicall deployed to : 0xBD6a373E0c80fF321FbD4A49e35527833040bf66
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,27 +6,27 @@ async function deploy() {
    deployerAddress = account.address;
    console.log(`Deploying contracts using ${deployerAddress}`);
 
-   //Deploy WETH
-   const weth = await ethers.getContractFactory('WETH');
-   const wethInstance = await weth.deploy();
+   // //Deploy WETH
+   // const weth = await ethers.getContractFactory('WETH');
+   // const wethInstance = await weth.deploy();
 
-   console.log(`WETH deployed to : ${wethInstance.address}`);
+   // console.log(`WETH deployed to : ${wethInstance.address}`);
 
-   //Deploy Factory
-   const factory = await ethers.getContractFactory('UniswapV2Factory');
-   const factoryInstance = await factory.deploy(deployerAddress);
+   // //Deploy Factory
+   // const factory = await ethers.getContractFactory('UniswapV2Factory');
+   // const factoryInstance = await factory.deploy(deployerAddress);
 
-   console.log(`Factory deployed to : ${factoryInstance.address}`);
+   // console.log(`Factory deployed to : ${factoryInstance.address}`);
 
-   //Deploy Router passing Factory Address and WETH Address
-   const router = await ethers.getContractFactory('UniswapV2Router02');
-   const routerInstance = await router.deploy(
-      factoryInstance.address,
-      wethInstance.address
-   );
-   await routerInstance.deployed();
+   // //Deploy Router passing Factory Address and WETH Address
+   // const router = await ethers.getContractFactory('UniswapV2Router02');
+   // const routerInstance = await router.deploy(
+   //    factoryInstance.address,
+   //    wethInstance.address
+   // );
+   // await routerInstance.deployed();
 
-   console.log(`Router V02 deployed to :  ${routerInstance.address}`);
+   // console.log(`Router V02 deployed to :  ${routerInstance.address}`);
 
    //Deploy Multicall (needed for Interface)
    const multicall = await ethers.getContractFactory('Multicall');
